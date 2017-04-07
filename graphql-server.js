9bd692fe010150769903f01c9cddbfd5139cac61
Andrea <andrea_lorente_1994@hotmail.com>
2017-04-07 14:34:45 +0200

    no funcionaaaa

--- graphql-server.js
+++ graphql-server.js
@@ -91,12 +91,12 @@ var mutationType = new graphql.GraphQLObjectType({
                 name: { type: graphql.GraphQLString },
 				email: {type: graphql.GraphQLString }
             },
-            resolve: function(_, { name, email }) {
+            resolve: function(_, args) {
                 return new Promise((resolve,reject) => {
                     
                     User.create({
-                        'name' : name,
-                        'email' : email
+                        'name' : args.name,
+                        'email' : args.email
                     }, function(err, res){
                         if(err) reject(err);
                         else{ resolve(res); } 
